aa5d909be293788f0b9c7c331fc9a0d1ced09f38
Nina <nina.korocevic@icloud.com>
2022-05-23 23:13:51 +0300

    fix: add new line char to fix new line problem  (Mon, May 23, 2022 11:13:51 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -7,7 +7,7 @@ const filePath = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(filePath, 'utf-8');
 let data = '';
 readStream.on('data', (chunk) => (data += chunk));
-readStream.on('end', () => stdout.write(data));
+readStream.on('end', () => stdout.write(data + '\n'));
 readStream.on('error', (error) =>
   stdout.write(`Something went wrong: ${error.message}\n`)
 );
